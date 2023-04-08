a7f04376c171861b8b16522224945be4ee3b1994
Peter Key <petekey@duck.com>
2023-04-09 08:28:25 +1000

    Add link tags to move around app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { BookList } from './pages/BookList';
 
 function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/books">Books</Link></li>
+          <li><Link to="/hi">Say hi</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/books" element={<BookList />} />
