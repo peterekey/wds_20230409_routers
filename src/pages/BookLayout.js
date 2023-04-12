a04a5a6a4b12010e4dbea3059f229e89accfd653
Peter Key <petekey@duck.com>
2023-04-13 08:41:13 +1000

    Nested routes with context

--- src/pages/BookLayout.js
+++ src/pages/BookLayout.js
@@ -6,7 +6,7 @@ export function BookLayout() {
             <Link to="/books/1">Book 1</Link><br/>
             <Link to="/books/2">Book 2</Link><br/>
             <Link to="/books/new">New book</Link>
-            <Outlet context={ { hello: "World"}} />
+            <Outlet context={ { hello: "World"} } />
         </>
     )
 }
