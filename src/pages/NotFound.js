4c2b019693cc4afeb2705a432f47f47fc6fb4bb1
Peter Key <petekey@duck.com>
2023-04-14 08:58:23 +1000

    useNavigate hook - redirect to specific page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,19 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function NotFound() {
-    return <Navigate to="/" />
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/")
+        }, 1000);
+    }, []);
+
+    return (
+        <>
+            <h1>NotFound</h1>
+            <p>Redirecting in a sec...</p>
+        </>
+    )
 }
